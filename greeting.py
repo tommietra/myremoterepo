851b881ea2907ead5a4918d9b399027ae299c44b
Tommie Tra <tommie.tra@assumption.edu>
2026-04-21 16:14:55 -0400

    Added os envirom HOME PATH access

--- greeting.py
+++ greeting.py
@@ -1,7 +1,10 @@
 import datetime import datetime
+import os
 
 print ( "Hello All" )
 
-print current time
+#print current time
 print (datetime.now())
 
+#print time directory path
+print (os_existing("Home"))
